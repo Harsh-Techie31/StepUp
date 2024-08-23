15ef08739790640f4a699403d69169c75daa3c81
Harsh - <harshchoudhary3113@gmail.com>
2024-08-23 21:14:04 +0530

    added the filters bar to chooose the brand from

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const [
+    'All',
+    'Adidas',
+    'Nike',
+    'Puma',
+    'Reebok',
+    'Skechers'
+  ];
+  late String selectedFilters;
+  @override
+  void initState() {
+    super.initState();
+    selectedFilters = filters[0];
+  }
 
   @override
   Widget build(BuildContext context) {
     const border = OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromRGBO(225, 225, 225, 1),
-                      ),
-                      borderRadius:
-                          BorderRadius.horizontal(left: Radius.circular(50)),
-                    );
-    return const Scaffold(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
+    );
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Row(
+            const Row(
               children: [
                 Padding(
                   padding: EdgeInsets.only(
@@ -35,11 +54,45 @@ class HomePage extends StatelessWidget {
                     prefixIcon: Icon(Icons.search),
                     enabledBorder: border,
                     focusedBorder: border,
-                    
                   )),
                 )
               ],
             ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: filters.length,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          selectedFilters = filter;
+                        });
+                        
+                      },
+                      child: Chip(
+                        label: Text(filter),
+                        labelStyle: const TextStyle(
+                          fontSize: 19,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 17, vertical: 12),
+                        backgroundColor: selectedFilters == filter ? Theme.of(context).colorScheme.primary : const Color.fromARGB(255, 221, 232, 243),
+                        side: const BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
       title: "StepUp-N0VA",
       theme: ThemeData(
         fontFamily: "cinzel",
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(254, 206, 1, 1),
+        primary: const Color.fromRGBO(254, 206, 1, 1)),
         inputDecorationTheme: const InputDecorationTheme(
           hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16,),
           prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
